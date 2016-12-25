c5645d4832cba598c996bd63b66b5f147cb40186
aandroth <aandroth@gmail.com>
2016-12-25 00:06:40 -0900

    Added the delete button and made the selecting better.

--- SecondAssessment/LAZERZLEVEL.h
+++ SecondAssessment/LAZERZLEVEL.h
@@ -12,6 +12,7 @@
 #include <vector>
 using std::vector;
 
+
 #include <iostream>
 using std::cout;
 
@@ -20,6 +21,7 @@ struct reflectionData
 	Vec2 lazerDirection, lazerEndPoint, objectLineOriginPoint, objectLineEndPoint;
 	bool isReflective;
 	float closenessOfCollision;
+	int lastMirrorIndex;
 };
 
 class LAZERZLEVEL
@@ -27,20 +29,23 @@ class LAZERZLEVEL
 	AABB m_aabbArr[20];
 	Box m_mirrors[20];
 	Transform m_mirrors_transforms[20];
-	bool m_mirror_bounced[20], targetIsInSight = false;
+	bool m_mirror_bounced[20], targetIsInSight = false, lazerIsFired = false, lazerIsOnTargetLine = false, mirrorMoving = false, mouseIsDown = false;
 	vector<Vec2>  m_bounceOriginAndDirections;
 	LAZERZCANNON m_lazerz_cannon;
 	LAZERZTARGET m_lazerz_target;
-	Button fire_lazerz_button;
-	size_t m_aabb_arr_size, m_mirrors_size, m_mirror_colors[20], indexOfSelectedMirror;
+	Button fire_lazerz_button, add_mirror_button, del_mirror_button;
+	size_t m_aabb_arr_size, m_mirrors_size, m_mirror_colors[20], indexOfSelectedMirror = -1;
 	unsigned m_font = sfw::loadTextureMap("./res/tonc_font.png", 16, 6);
 	unsigned int mouse_image = sfw::loadTextureMap("./Images/LAZERZ_Button.png", 16, 6);
+	float timePassed;
+	Vec2 currLazerOrigin, currLazerDir, currLazerEnd, progress, currMousePos;
+	reflectionData nextLazer;
 
 public:
 	LAZERZLEVEL(); 
 	reflectionData lazerzCollisionDetectionTarget(Vec2 lineDirection, Vec2 linePosition);
 	reflectionData lazerzCollisionDetection(Vec2 lineDirection, Vec2 linePosition);
-	reflectionData lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2 linePosition);
+	reflectionData lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2 linePosition, int lastMirroIndex);
 	int MirrorMouseCollisionDetection();
 	void init();
 	void draw();
@@ -49,9 +54,17 @@ public:
 
 LAZERZLEVEL::LAZERZLEVEL()
 {
-	fire_lazerz_button.m_pos = Vec2(850, 900);
+	fire_lazerz_button.m_pos = Vec2(600, 900);
 	fire_lazerz_button.m_dim = Vec2(100, 50);
 	fire_lazerz_button.m_texture = ("./Images/LAZERZ_Button.png", 16, 6);
+
+	add_mirror_button.m_pos = Vec2(750, 900);
+	add_mirror_button.m_dim = Vec2(100, 50);
+	add_mirror_button.m_texture = ("./Images/LAZERZ_Button.png", 16, 6);
+
+	del_mirror_button.m_pos = Vec2(900, 900);
+	del_mirror_button.m_dim = Vec2(100, 50);
+	del_mirror_button.m_texture = ("./Images/LAZERZ_Button.png", 16, 6);
 
 	m_aabbArr[0] = AABB(500,  50, 500,  50);   // Ground
 	m_aabbArr[1] = AABB(  0, 550, 150, 450);   // Left
@@ -62,13 +75,17 @@ LAZERZLEVEL::LAZERZLEVEL()
 
 	m_mirrors[0] = Box(0, 0, 20, 200);
 	m_mirrors_transforms[0].m_facing = -30;
-	m_mirrors_transforms[0].translateByMat3 = translate(880, 110);
+	m_mirrors_transforms[0].m_position = Vec2(880, 110);
 	m_mirror_colors[0] = WHITE;
 
 	m_mirrors[1] = Box(0, 0, 20, 200);
-	m_mirrors_transforms[1].translateByMat3 = translate(800, 500);
+	m_mirrors_transforms[1].m_position = Vec2(800, 350);
 	m_mirror_colors[1] = WHITE;
-	m_mirrors_size = 2;
+
+	m_mirrors[2] = Box(0, 0, 20, 200);
+	m_mirrors_transforms[2].m_position = Vec2(840, 350);
+	m_mirror_colors[2] = WHITE;
+	m_mirrors_size = 3;
 
 	m_lazerz_target.init();
 }
@@ -177,7 +194,7 @@ reflectionData LAZERZLEVEL::lazerzCollisionDetection(Vec2 lineDirection, Vec2 li
 	return reflectData;
 }
 
-reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2 linePosition)
+reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2 linePosition, int lastMirrorIndex)
 {
 	linePosition += normal(lineDirection);
 	float line_dot_product = dot(lineDirection, linePosition),
@@ -191,6 +208,9 @@ reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2
 
 	for (int ii = 0; ii < m_mirrors_size; ++ii)
 	{
+		if (ii == lastMirrorIndex)
+			continue;
+
 		float aMin, aMax;
 		currMirror = m_mirrors_transforms[ii].getGlobalTransform() * m_mirrors[ii];
 
@@ -215,6 +235,19 @@ reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2
 				// Check if the collision point is closest to the laser without going past it
 				float dotProduct_1 = dot(lineDirection, collPoint);
 
+				//if ((ii == 1 && jj == 2) || (ii == 2 && jj == 0))
+				//{
+				//	//cout << "idx: " << ii << ", " << jj << "\n";
+				//	//cout << "velLine: " << velLine.x << ", " << velLine.y << "\n";
+				//	//cout << "planeLine: " << planeLine.x << ", " << planeLine.y << "\n";
+				//	//cout << "collPoint: " << collPoint.x << ", " << collPoint.y << "\n";
+				//	//cout << "lineDirection: " << lineDirection.x << ", " << lineDirection.y << "\n";
+				//	//cout << "linePosition: " << linePosition.x << ", " << linePosition.y << "\n";
+				//	//cout << "closestCollPointCloseness: " << closestCollPointCloseness << "\n";
+				//	//cout << "dotProduct_1: " << dotProduct_1 << "\n";
+				//	//cout << "line_dot_product: " << line_dot_product << "\n\n";
+				//}
+
 				if (dotProduct_1 >= line_dot_product && dotProduct_1 < closestCollPointCloseness)
 				{
 					closestCollPointCloseness = dotProduct_1;
@@ -225,6 +258,7 @@ reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2
 					reflectData.objectLineEndPoint    = lineEnd;
 					reflectData.lazerDirection = lineDirection;
 					reflectData.closenessOfCollision = closestCollPointCloseness;
+					reflectData.lastMirrorIndex = ii;
 				}
 			}
 		}
@@ -236,11 +270,18 @@ reflectionData LAZERZLEVEL::lazerzCollisionDetectionBox(Vec2 lineDirection, Vec2
 void LAZERZLEVEL::init()
 {
 	m_lazerz_cannon.positionCannon(Vec2(200, 120));
+	timePassed = 0.0;
+	currLazerOrigin = m_lazerz_cannon.lazerzOrigin();
+	currLazerDir = m_lazerz_cannon.lazerzDirection();
+	nextLazer = lazerzCollisionDetectionBox(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin(), -1);
+	currLazerEnd = nextLazer.lazerEndPoint;
 }
 
 void LAZERZLEVEL::draw()
 {
 	fire_lazerz_button.draw();
+	add_mirror_button.draw();
+	del_mirror_button.draw();
 
 	for (int ii = 0; ii < m_aabb_arr_size; ++ii)
 	{
@@ -254,48 +295,110 @@ void LAZERZLEVEL::draw()
 
 	m_lazerz_target.draw();
 
-	reflectionData hardCollisionData = lazerzCollisionDetection(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin());
-	reflectionData targetCollisionData = lazerzCollisionDetectionTarget(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin());
-	reflectionData p = lazerzCollisionDetectionBox(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin());
-	Vec2 lazerOrigin = m_lazerz_cannon.lazerzOrigin();
-	Vec2 reflectVec = reflection(perp(p.objectLineOriginPoint - p.objectLineEndPoint), p.lazerDirection * 2000);
-	if(hardCollisionData.closenessOfCollision < p.closenessOfCollision && hardCollisionData.closenessOfCollision < targetCollisionData.closenessOfCollision)
-		sfw::drawLine(lazerOrigin.x, lazerOrigin.y, hardCollisionData.lazerEndPoint.x, hardCollisionData.lazerEndPoint.y, BLUE);
-	else if (targetCollisionData.closenessOfCollision < p.closenessOfCollision)
+	if (!lazerIsFired)
 	{
-		sfw::drawLine(lazerOrigin.x, lazerOrigin.y, targetCollisionData.lazerEndPoint.x, targetCollisionData.lazerEndPoint.y, BLUE);
-	}
-	else
-	{
-		int limit = 20, ii = 0;
-		while (p.lazerEndPoint.x != -INFINITY && ii < limit)
+		reflectionData hardCollisionData = lazerzCollisionDetection(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin());
+		reflectionData targetCollisionData = lazerzCollisionDetectionTarget(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin());
+		reflectionData p = lazerzCollisionDetectionBox(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin(), -1);
+		//cout << "p.lazerEndPoint: " << p.lazerEndPoint.x << ", " << p.lazerEndPoint.y << "\n";
+		Vec2 lazerOrigin = m_lazerz_cannon.lazerzOrigin();
+		Vec2 reflectVec = reflection(perp(p.objectLineOriginPoint - p.objectLineEndPoint), p.lazerDirection * 2000);
+		int limit = 10, ii = 0;
+		if (p.closenessOfCollision < hardCollisionData.closenessOfCollision && 
+			p.closenessOfCollision < targetCollisionData.closenessOfCollision)
 		{
-			sfw::drawLine(lazerOrigin.x, lazerOrigin.y, p.lazerEndPoint.x, p.lazerEndPoint.y, YELLOW);
+			while (p.lazerEndPoint.x != -INFINITY && ii < limit)
+			{
+				sfw::drawLine(lazerOrigin.x, lazerOrigin.y, p.lazerEndPoint.x, p.lazerEndPoint.y, YELLOW);
 
-			lazerOrigin = p.lazerEndPoint;
-			reflectVec = reflection(perp(p.objectLineOriginPoint - p.objectLineEndPoint), p.lazerDirection * 2000);
-			p = lazerzCollisionDetectionBox(reflectVec, lazerOrigin);
+				lazerOrigin = p.lazerEndPoint;
+				reflectVec = reflection(perp(p.objectLineOriginPoint - p.objectLineEndPoint), p.lazerDirection * 2000);
+				//cout << "reflectVec: " << reflectVec.x << ", " << reflectVec.y << "\n";
+				//cout << "p.closenessOfCollision: " << p.closenessOfCollision << "\n";
+				//cout << "p.lazerDirection: " << p.lazerDirection.x << ", " << p.lazerDirection.y << "\n\n";
+				p = lazerzCollisionDetectionBox(reflectVec, p.lazerEndPoint, p.lastMirrorIndex);
+				//cout << "p.lazerEndPoint: " << p.lazerEndPoint.x << ", " << p.lazerEndPoint.y << "\n";
 
-			hardCollisionData = lazerzCollisionDetection(reflectVec, lazerOrigin);
-			targetCollisionData = lazerzCollisionDetectionTarget(reflectVec, lazerOrigin);
-			if (hardCollisionData.closenessOfCollision < p.closenessOfCollision || targetCollisionData.closenessOfCollision < p.closenessOfCollision)
-				break;
-			++ii;
+				hardCollisionData = lazerzCollisionDetection(reflectVec, lazerOrigin);
+				targetCollisionData = lazerzCollisionDetectionTarget(reflectVec, lazerOrigin);
+				if (hardCollisionData.closenessOfCollision < p.closenessOfCollision || targetCollisionData.closenessOfCollision < p.closenessOfCollision)
+					break;
+				++ii;
+
+				//cout << "hardCollisionData.lazerEndPoint: " << hardCollisionData.lazerEndPoint.x << ", " << hardCollisionData.lazerEndPoint.y << "\n";
+				//cout << "targetCollisionData.lazerEndPoint: " << targetCollisionData.lazerEndPoint.x << ", " << targetCollisionData.lazerEndPoint.y << "\n\n";
+			}
+			//cout << "\n";
+		}
+		else if (targetCollisionData.closenessOfCollision < hardCollisionData.closenessOfCollision)
+		{
+			sfw::drawLine(lazerOrigin.x, lazerOrigin.y, targetCollisionData.lazerEndPoint.x, targetCollisionData.lazerEndPoint.y, BLUE);
+		}
+		else
+		{
+			sfw::drawLine(lazerOrigin.x, lazerOrigin.y, hardCollisionData.lazerEndPoint.x, hardCollisionData.lazerEndPoint.y, BLUE);
+			targetIsInSight = false;
+		}
+
+
+
+		//if (!reflectVec.x)
+		//	reflectVec = m_lazerz_cannon.lazerzDirection();
+		if (ii != limit)
+		{
+			if (hardCollisionData.closenessOfCollision < targetCollisionData.closenessOfCollision)
+			{
+				sfw::drawLine(lazerOrigin.x, lazerOrigin.y, hardCollisionData.lazerEndPoint.x, hardCollisionData.lazerEndPoint.y, YELLOW);
+			}
+			else
+			{
+				sfw::drawLine(lazerOrigin.x, lazerOrigin.y, targetCollisionData.lazerEndPoint.x, targetCollisionData.lazerEndPoint.y, BLUE);
+				targetIsInSight = true;
+			}
 		}
 	}
-
-	if (!reflectVec.x)
-		reflectVec = m_lazerz_cannon.lazerzDirection();
-
-	if (hardCollisionData.closenessOfCollision < targetCollisionData.closenessOfCollision)
+	//////////////////////////////////////////////////////////////
+	else //if (lazerIsFired)
 	{
-		sfw::drawLine(lazerOrigin.x, lazerOrigin.y, hardCollisionData.lazerEndPoint.x, hardCollisionData.lazerEndPoint.y, BLUE);
-		targetIsInSight = false;
-	}
-	else
-	{
-		sfw::drawLine(lazerOrigin.x, lazerOrigin.y, targetCollisionData.lazerEndPoint.x, targetCollisionData.lazerEndPoint.y, BLUE);
-		targetIsInSight = true;
+		Vec2 unit(normal(currLazerDir)), length(normal(currLazerDir) * 50.0);
+		if (magnitude(unit * timePassed) < magnitude(currLazerEnd - currLazerOrigin))
+		{
+			timePassed += sfw::getDeltaTime() * 1000;
+			progress = currLazerOrigin + (unit * timePassed);
+
+			if (magnitude((progress + length) - currLazerOrigin) < magnitude(currLazerEnd - currLazerOrigin))
+			{
+				sfw::drawLine(progress.x, progress.y,
+					progress.x + length.x, progress.y + length.y, RED);
+			}
+			else
+			{
+				sfw::drawLine(progress.x, progress.y,
+					currLazerEnd.x, currLazerEnd.y, RED);
+
+				Vec2 r = reflection(perp(nextLazer.objectLineOriginPoint - nextLazer.objectLineEndPoint), nextLazer.lazerDirection * 2000);
+				Vec2 leftOverLineEnd(normal(r) * magnitude((progress + length) - currLazerEnd));
+
+				sfw::drawLine(currLazerEnd.x, currLazerEnd.y,
+					currLazerEnd.x + leftOverLineEnd.x, currLazerEnd.y + leftOverLineEnd.y, RED);
+
+				if(lazerIsOnTargetLine)
+					m_lazerz_target.m_deathSequenceIsActive = true;
+			}
+		}
+		else
+		{
+			timePassed = 0.0;
+			currLazerOrigin = currLazerEnd;
+			currLazerDir = reflection(perp(nextLazer.objectLineOriginPoint - nextLazer.objectLineEndPoint), nextLazer.lazerDirection * 2000);
+			nextLazer = lazerzCollisionDetectionBox(currLazerDir, currLazerOrigin, nextLazer.lastMirrorIndex);
+			if (nextLazer.lazerEndPoint.x == -INFINITY)
+			{
+				nextLazer = lazerzCollisionDetectionTarget(currLazerDir, currLazerOrigin);
+				lazerIsOnTargetLine = true;
+			}
+			currLazerEnd = nextLazer.lazerEndPoint;
+		}
 	}
 
 	m_lazerz_cannon.draw();
@@ -353,31 +456,91 @@ void LAZERZLEVEL::step()
 	{
 		m_mirrors_transforms[indexOfSelectedMirror].rotateLocalTransform(-0.5);
 	}
-	if (sfw::getKey('W'))
-	{
-		m_mirrors_transforms[1].rotateLocalTransform(0.5);
-	}
-	else if (sfw::getKey('S'))
-	{
-		m_mirrors_transforms[1].rotateLocalTransform(-0.5);
-	}
 	
 	m_lazerz_target.step();
+
 	if (fire_lazerz_button.mouseUp() && targetIsInSight)
 	{
-		m_lazerz_target.m_deathSequenceIsActive = true;
+		lazerIsFired = true;
+		timePassed = 0.0;
+		currLazerOrigin = m_lazerz_cannon.lazerzOrigin();
+		currLazerDir = m_lazerz_cannon.lazerzDirection();
+		nextLazer = lazerzCollisionDetectionBox(m_lazerz_cannon.lazerzDirection(), m_lazerz_cannon.lazerzOrigin(), -1);
+		currLazerEnd = nextLazer.lazerEndPoint;
 	}
 	fire_lazerz_button.mouseDown();
+	if (add_mirror_button.mouseUp())
+	{
+		if (m_mirrors_size < 10)
+		{
+			m_mirrors[m_mirrors_size] = Box(0, 0, 20, 200);
+			m_mirrors_transforms[m_mirrors_size].m_facing = 0;
+			m_mirrors_transforms[m_mirrors_size].m_position = Vec2(500, 500);
+			m_mirror_colors[m_mirrors_size] = WHITE;
+			++m_mirrors_size;
+		}
+	}
+	add_mirror_button.mouseDown();
+	if (del_mirror_button.mouseUp())
+	{
+		if (m_mirrors_size > 0 && indexOfSelectedMirror != -1)
+		{
+			for (int ii = indexOfSelectedMirror; ii < m_mirrors_size-1; ++ii)
+			{
+				cout << "here\n";
+				m_mirrors_transforms[ii].m_facing = m_mirrors_transforms[ii + 1].m_facing;
+				m_mirrors_transforms[ii].m_position = m_mirrors_transforms[ii + 1].m_position;
+			}
+			m_mirrors_transforms[m_mirrors_size - 1].m_facing = 0;
+			m_mirrors_transforms[m_mirrors_size - 1].m_position = Vec2(0, 0);
+			m_mirror_colors[indexOfSelectedMirror] = WHITE;
+			--m_mirrors_size;
+			indexOfSelectedMirror = -1;
+		}
+	}
+	del_mirror_button.mouseDown();
 
 	if (sfw::getMouseButton(0))
 	{
 		for (int ii = 0; ii < m_mirrors_size; ++ii)
-			m_mirror_colors[ii] = WHITE;
+		{
+			if(ii != indexOfSelectedMirror)
+				m_mirror_colors[ii] = WHITE;
+		}
 
-		indexOfSelectedMirror = MirrorMouseCollisionDetection();
-		if(indexOfSelectedMirror != -1)
+		if (indexOfSelectedMirror == MirrorMouseCollisionDetection())
+			mirrorMoving = true;
+
+		if (indexOfSelectedMirror != -1 && mirrorMoving)
+		{
+			m_mirrors_transforms[indexOfSelectedMirror].m_position += Vec2(sfw::getMouseX() - currMousePos.x, sfw::getMouseY() - currMousePos.y);
+			currMousePos = Vec2(sfw::getMouseX(), sfw::getMouseY());
+		}
+		else if(!mouseIsDown && MirrorMouseCollisionDetection() != -1)
+		{
+			indexOfSelectedMirror = MirrorMouseCollisionDetection();
 			m_mirror_colors[indexOfSelectedMirror] = YELLOW;
+			currMousePos = Vec2(sfw::getMouseX(), sfw::getMouseY());
+		}
+
+		mouseIsDown = true;
 	}
+	else
+	{
+		if (mouseIsDown) // mouse just came up
+		{
+			mouseIsDown = false;
+			if(MirrorMouseCollisionDetection() == -1 && indexOfSelectedMirror != -1)
+			{
+				m_mirror_colors[indexOfSelectedMirror] = WHITE;
+				indexOfSelectedMirror = -1;
+			}
+
+		}
+		currMousePos = Vec2(sfw::getMouseX(), sfw::getMouseY());
+		mirrorMoving = false;
+	}
+
 }
 
 //LAZERZMENU LAZERZLEVEL::next()
